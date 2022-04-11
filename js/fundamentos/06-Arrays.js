69cb644064de932579169efcdaca78f1682c116a
Rocío Dorado <rocio.dorado.12@hotmail.com>
2022-04-11 16:07:31 -0300

    Agrego tareas hecha en clase en el archivo de arreglo

--- js/fundamentos/06-Arrays.js
+++ js/fundamentos/06-Arrays.js
@@ -108,3 +108,57 @@ if(lista.includes(numero)){
 } else {
     console.log(`El número ingresado no está en la lista`);
 }
+
+/*Tarea 3:
+Declara un array que vamos a llamar “clasificaciones” con los siguientes valores:
+Ana, Oswaldo, Raúl, Celia, María, Antonio
+(vamos a suponer que es el orden de clasificación de un concurso, en un momento dado)
+
+- Imprime la clasificación actual
+El concurso continua, y se van modifican esas posiciones anteriores. Debemos cambiar en el array:
+
+- Celia adelanta a Raúl
+- Antonio es descalificado y se elimina del concurso
+- Detrás de Ana y antes de Oswaldo se clasifican dos nuevos concursantes: Roberto y Amaya, en ese orden
+- Hay una nueva participante que pasa a encabezar la clasificación: Marta
+- Imprime la clasificación actualizada y comprueba que se ha hecho correctamente
+*/
+
+let clasificaciones = [];
+clasificaciones.push('Ana', 'Oswaldo', 'Raúl', 'Celia', 'María', 'Antonio');
+console.log(clasificaciones[3]);
+
+//Celia adelanta a Raúl
+clasificaciones.splice(2,2,"Celia", "Raúl");
+
+//Antonio es descalificado y se elimina del concurso
+clasificaciones.pop();
+
+//Detrás de Ana y antes de Oswaldo se clasifican dos nuevos concursantes: Roberto y Amaya, en ese orden
+clasificaciones.splice(1,0, 'Roberto', 'Amaya');
+
+//Hay una nueva participante que pasa a encabezar la clasificación: Marta
+clasificaciones.unshift('Marta');
+
+//Imprime la clasificación actualizada y comprueba que se ha hecho correctamente
+console.log(clasificaciones);
+
+//--------
+
+/*Tarea 4: 
+- Pedir por pantalla el nombre de 5 alumnos de la comisión. Usar ciclo while
+- Guardarlos en un arreglo llamado rollingCoders
+- Usando el arreglo mostrar en consola por cada uno un mensaje de bienvenida al mundo del desarrollo web.
+
+*/
+
+let rollingCoders = [];
+
+while(rollingCoders.length < 5){
+   //acciones
+   rollingCoders.push(prompt("Ingrese el nombre del alumno"));
+}
+
+for(let i = 0; i < rollingCoders.length; i++){
+    console.log(`Hola ${rollingCoders[i]} Bienvenido al mundo del desarrollo web`);
+}
